15e768b446b48f474810be59039647d87ce1eaea
Anantha Raju C <arcswdev@gmail.com>
2021-10-17 11:48:22 +0530

    Built-In @FunctionalInterfaces with Lambdas

--- src/main/java/io/github/anantharajuc/j8f/Java8Application.java
+++ src/main/java/io/github/anantharajuc/j8f/Java8Application.java
@@ -15,16 +15,57 @@ public class Java8Application
 	{
 		SpringApplication.run(Java8Application.class, args);
 		
-		/*Functional interface - No Arguments, No Return Value*/
-		FInoArgumentsNoReturnValue finanrv = () -> log.info("Functional interface - No Arguments, No Return Value");
+	   /*
+		* 1. Functional interface - No Arguments, No Return Value
+		*
+		*/
+		FInoArgumentsNoReturnValue finanrv = () -> log.info("1. Functional interface - No Arguments, No Return Value.");
 		finanrv.doSomething();
 		
-		/*Functional interface - 2 Arguments, No Return Value*/
+	   /*
+		* 2. Functional interface - 2 Arguments, No Return Value
+		*
+		*/
 		FI2ArgumentsNoReturnValue fi2anrv = (v1, v2) -> {
 															int result = v1+v2;
-															log.info("Sum of v1, v2 = "+result);
+															log.info("2. Sum of v1, v2 = "+result+".");
 														};
-		fi2anrv.calculate(3, 20);	 											
+		fi2anrv.calculate(3, 20);	 			
+		
+	   /*
+		* 3. Built-In Interface - Part of the Java Run Time - Interface part of the Multi-Threaded Architecture of Java.
+		*
+		*/		
+		Runnable r1 = new Runnable()
+									{   //Single Abstract Method - run()
+										@Override
+										public void run()
+										{
+											log.info("3. Running thread r1 via Inner Class.");
+										}
+									};
+									
+	    new Thread(r1).start();
+	    
+	   /*
+		* 4. Implementing Built-In Interface with Lambda Expressions.
+		*
+		*/
+	    Runnable r2 = () -> {
+	    						try 
+	    						{
+									Thread.sleep(1000);
+								} 
+	    						catch (InterruptedException e) 
+	    						{
+	    							log.error("Thread Interrupted Exception", e);
+									Thread.currentThread().interrupt();
+								} 
+	    						
+	    						log.info("4. Running thread r2 via Lambda Expressions.");
+	    					};
+	    					
+	    new Thread(r2).start(); 
 	}
 }
  
